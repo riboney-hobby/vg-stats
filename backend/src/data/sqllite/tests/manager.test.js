3fcc24fefa9a5674223e9c9aba96781d4205fe73
ribone <14286113+riboney@users.noreply.github.com>
2021-12-07 18:33:25 -0500

    Adds test class for sqlite.manager.js

--- backend/src/data/sqllite/tests/manager.test.js
+++ backend/src/data/sqllite/tests/manager.test.js
@@ -1,1 +1,17 @@
-dasfds
+const SqliteManager = require('../sqlite.manager')
+
+describe('Tests database creation and closure', () => {
+
+    //const dbManager = new SqliteManager();
+
+    // test('tests createDatabase()', () => {
+    //     const result = dbManager.createDatabase();
+    //     expect(result).toEqual({author: 'Edsger W. Dijkstra', blogs: 2})
+    // })
+
+    test('console log test', () => {
+        console.log = jest.fn();
+        log('hello');
+        expect(console.log.mock.calls[0][0]).toBe('hello');
+    })
+})
